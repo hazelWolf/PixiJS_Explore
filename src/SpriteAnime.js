e6f309713263ce1b73b1218494defc6b535a8deb
bhomitb <bhomitb@yahoo.com>
2018-04-24 14:34:27 +0530

    hazelWolf

--- src/SpriteAnime.js
+++ src/SpriteAnime.js
@@ -20,7 +20,9 @@ renderer.view.style.display = "block";
 renderer.autoResize = true;
 renderer.resize(window.innerWidth, window.innerHeight);
 //mario //s3.amazonaws.com/media-p.slid.es/uploads/55786/images/1828075/mario-jump.png
-loader.add("megaman", "../assets/kame.png").load(setup);
+loader.add("megaman", "../assets/compileBird.png")
+.add("bg", )
+.load(setup);
 
 var megaman;
 var run = [];
@@ -28,28 +30,26 @@ var megamanTexture;
 
 function setup() {
   megamanTexture = loader.resources["megaman"].texture;
-  run.push (new PIXI.Rectangle(0,0,90,175),
-           new PIXI.Rectangle(90,0,90,175),
-           new PIXI.Rectangle(190,0,90,175),
-           new PIXI.Rectangle(290,0,90,175),
-           new PIXI.Rectangle(190,0,90,175));
+  run.push (new PIXI.Rectangle(0,0,160,160),
+              new PIXI.Rectangle(170,0,160,160),
+              new PIXI.Rectangle(330,0,160,160),
+              new PIXI.Rectangle(490,0,90,160),
+              new PIXI.Rectangle(630,0,90,160)
+        );
   megamanTexture.frame = run[0];
   megaman = new Sprite(megamanTexture);
   megaman.x=window.innerWidth/2;
   megaman.y=window.innerHeight/2;
   stage.addChild(megaman);
   renderer.render(stage);
-  
-  
 }
-
 function gameLoop() {
   var f = requestAnimationFrame(gameLoop);
   if (af >= maxFrame)
   {
     af = 0;
   } else {
-    af += 1/df;
+    af += 0.2/df;
   }
   megamanTexture.frame = run[Math.floor(af)];
   stage.removeChild(megaman);
