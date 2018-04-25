cb49af37e8cff7616aa76ebb1a3c5385f5f20fb4
bhomitb <bhomitb@yahoo.com>
2018-04-26 02:08:55 +0530

    level 1 complete

--- src/GhostAttacker.js
+++ src/GhostAttacker.js
@@ -4,6 +4,7 @@ app.view.style.display = "block";
 document.body.appendChild(app.view)
 
 var bg = new PIXI.Sprite.fromImage("../assets/BG.png");
+
 bg.width = app.screen.width;
 bg.height = app.screen.height;
 app.stage.addChild(bg);
@@ -15,14 +16,18 @@ var gameloader = new PIXI.loaders.Loader();
 var vx = 5 ;
 var vy = 5;
 var flag = 0;
+//Score
+var count =0;
+var score;
 var gameState = 0; 
 //Bhoot awaz
 const sound =  PIXI.sound.Sound.from({url :'../assets/hauntedhouse.wav', loop:true});
 sound.play();
-
+const soundPlay =  PIXI.sound.Sound.from({url :'../assets/attack.wav', loop:true});
 //Anime holders
 const idleContainer = new PIXI.Container();
 const muteContain = new PIXI.Container();
+const pumpContainer = new PIXI.Container();
 const gameContainer = new PIXI.Container();
 muteContain.width = 50;
 muteContain.height = 50;
@@ -48,23 +53,33 @@ gameControButton.buttonMode = true;
 
 function onStartStop(){
     if(flag == 0){
+        count=0;
+        sound.stop();
+        soundPlay.play();
         gameState = 1;
         this.texture =  buttonDown;
         console.log("start");
-        Idleloader.destroy();
         app.stage.removeChild(idleContainer)
-        StartGame();
+        app.stage.addChild(gameContainer);
         flag=1;
+        //Pumpkin saga
+        var pumpkin = new PIXI.Sprite.fromImage("../assets/Pumpkin.png");
+        pumpkin.height = 100;
+        pumpkin.width = 100;
+        pumpContainer.addChild(pumpkin);
+        
 }
     else{
        if(flag == 1){
+        soundPlay.stop();
+        sound.play();
         gameState = 0;
         this.texture = buttonIdeal;
-        flag = 0
+        gameloader.destroy(true,true);
         app.stage.removeChild(gameContainer);
-        gameloader.destroy();
+        app.stage.addChild(idleContainer);
         console.log("stop");
-        idler();
+        flag = 0
        }
     }
 }
@@ -88,13 +103,22 @@ var mute =false;
 mute = true;
 this.texture = muteOnTexture;
 sound.stop();
+soundPlay.stop();
      }
      else{
          mute = false
          this.texture = muteTexture;
+         if(gameState==1){
+             soundPlay.play()
+         }
+         else{
          sound.play();
+         }
      }
  }
+
+ app.stage.addChild(buttoncontainer)
+app.stage.addChild(muteContain);
 function idler(){
  Idleloader.add('../assets/data/ideal.json').load(onAssetLoaded)
 function onAssetLoaded(){
@@ -118,22 +142,22 @@ function onAssetLoaded(){
     anim.x = app.screen.width / 2;
     anim.y = app.screen.height-200 ;
     anim.anchor.set(0.5);
+    anim.height = 550;
+    anim.width =900;
     anim.animationSpeed = 0.5;
     anim.play();
 
     idleContainer.addChild(anim);
-
-    // Animate the rotation
-    //  app.ticker.add(function() {
-    //     anim.rotation += 0.01;
-    // });
-    app.stage.addChild(buttoncontainer)
-    app.stage.addChild(muteContain);
     app.stage.addChild(idleContainer);
+    score = new PIXI.Text("Score : " + count, style);
+                    score.x = window.innerWidth-400;
+                    score.y = 70;
+                    idleContainer.addChild(score);
  }
 }
  function StartGame(){
-    idleContainer.destroy({children:true, texture:true, baseTexture:true})
+    let left = keyboard(37),
+    right = keyboard(39);
     gameloader.add('../assets/data/attack.json').load(onAttackLoaded)
     function onAttackLoaded(){
         var framesA = [];
@@ -155,19 +179,188 @@ function onAssetLoaded(){
         animA.x = app.screen.width / 2;
         animA.y = app.screen.height-200 ;
         animA.anchor.set(0.5);
+        animA.height = 300;
+        animA.width = 250;
         animA.animationSpeed = 0.5;
         animA.play();
-    
+        gameContainer.addChild(pumpContainer);
         gameContainer.addChild(animA);
+        pumpContainer.x = 500;
     
         // Animate the rotation
           app.ticker.add(function() {
-       
-           animA.x += 1;
-            
-            
+            if(left.isDown && animA.x > 120){
+           animA.x -= 15;
+            }
+            if(right.isDown && animA.x < window.innerWidth-150){
+                animA.x += 15;
+                 }
+                 pumpContainer.y += 4;
+                 if (pumpContainer.y >=window.innerHeight-100) {
+                    pumpContainer.y = 0;
+                    pumpContainer.x = randomIntFromInterval(200,window.innerWidth);
+                 }
+                if(hitTestRectangle(animA,pumpContainer)){
+                    gameContainer.removeChild(score);
+                    console.log("hit");
+                    gameContainer.removeChild(pumpContainer);
+                    count ++;
+                    gameContainer.addChild(pumpContainer);
+                    pumpContainer.y = 0;
+                    pumpContainer.x = randomIntFromInterval(200,window.innerWidth);
+                    score = new PIXI.Text("Score : " + count, style);
+                    score.x = window.innerWidth-400;
+                    score.y = 70;
+                    gameContainer.addChild(score);
+                }
+                
          });
-         app.stage.addChild(gameContainer);
+         
+         
     }
 }
-idler();
+idler()
+StartGame()
+app.stage.addChild(idleContainer);
+//Score
+
+var style = new PIXI.TextStyle({
+    fontFamily: 'Luminari',
+    fontSize: 60,
+    fontStyle: 'italic',
+    fontWeight: 'bold',
+    fill: ['#ffffff', '#b2f6ff'], // gradient
+    stroke: '#4a1850',
+    strokeThickness: 5,
+    dropShadow: true,
+    dropShadowColor: '#000000',
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
+    wordWrap: true,
+    wordWrapWidth: 440
+});
+
+
+
+
+//Keyboard tracking
+function keyboard(keyCode) {
+    let key = {};
+    key.code = keyCode;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+    //The `downHandler`
+    key.downHandler = event => {
+      if (event.keyCode === key.code) {
+        if (key.isUp && key.press) key.press();
+        key.isDown = true;
+        key.isUp = false;
+      }
+      event.preventDefault();
+    };
+  
+    //The `upHandler`
+    key.upHandler = event => {
+      if (event.keyCode === key.code) {
+        if (key.isDown && key.release) key.release();
+        key.isDown = false;
+        key.isUp = true;
+      }
+      event.preventDefault();
+    };
+  
+    //Attach event listeners
+    window.addEventListener(
+      "keydown", key.downHandler.bind(key), false
+    );
+    window.addEventListener(
+      "keyup", key.upHandler.bind(key), false
+    );
+    return key;
+  }
+  //Toggle fullscreen
+  
+ var fullscreen = new PIXI.Sprite.fromImage("../assets/fullscreen.png");
+
+ fullscreen.buttonMode = true;
+ fullscreen.interactive = true;
+fullscreen.x = window.innerWidth- 100;
+fullscreen.y = window .innerHeight - 70;
+fullscreen.height = 55;
+fullscreen.width = 55;
+     fullscreen.on('click', ()=>{
+        if(document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if(document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if(document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen();
+        } else if(document.documentElement.msRequestFullscreen) {
+            document.documentElement.msRequestFullscreen();
+        }
+     })
+     app.stage.addChild(fullscreen);
+
+     window.onresize = function(){
+        app.renderer.resize(window.innerWidth, window.innerHeight);
+     }
+     function randomIntFromInterval(min,max)
+     {
+         return Math.floor(Math.random()*(max-min+1)+min);
+     }
+
+    //Collision Detection
+    function hitTestRectangle(r1, r2) {
+
+        //Define the variables we'll need to calculate
+        let hit, combinedHalfWidths, combinedHalfHeights, vx, vy;
+      
+        //hit will determine whether there's a collision
+        hit = false;
+      
+        //Find the center points of each sprite
+        r1.centerX = r1.x + r1.width / 2;
+        r1.centerY = r1.y + r1.height / 2;
+        r2.centerX = r2.x + r2.width / 2;
+        r2.centerY = r2.y + r2.height / 2;
+      
+        //Find the half-widths and half-heights of each sprite
+        r1.halfWidth = r1.width / 2;
+        r1.halfHeight = r1.height / 2;
+        r2.halfWidth = r2.width / 2;
+        r2.halfHeight = r2.height / 2;
+      
+        //Calculate the distance vector between the sprites
+        vx = r1.centerX - r2.centerX;
+        vy = r1.centerY - r2.centerY;
+      
+        //Figure out the combined half-widths and half-heights
+        combinedHalfWidths = r1.halfWidth + r2.halfWidth;
+        combinedHalfHeights = r1.halfHeight + r2.halfHeight;
+      
+        //Check for a collision on the x axis
+        if (Math.abs(vx) < combinedHalfWidths) {
+      
+          //A collision might be occuring. Check for a collision on the y axis
+          if (Math.abs(vy) < combinedHalfHeights) {
+      
+            //There's definitely a collision happening
+            hit = true;
+          } else {
+      
+            //There's no collision on the y axis
+            hit = false;
+          }
+        } else {
+      
+          //There's no collision on the x axis
+          hit = false;
+        }
+      
+        //`hit` will be either `true` or `false`
+        return hit;
+      };
+  
